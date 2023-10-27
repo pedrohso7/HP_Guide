9b615bcceea4e537eeae02b79345ff211dcef473
Pedro Oliveira <pholiveira1998@hotmail.com>
2023-10-27 12:26:49 -0400

    chore: remove unused files from core folder.

--- lib/core/constants/errors/validators_errors.dart
+++
@@ -1,9 +0,0 @@
-enum ValidatorsErrors {
-  emptyField('Campo obrigatório'),
-  invalidEmail('Email inválido'),
-  passwordsAreNotEqual('Senhas diferem.'),
-  weakPassword('Sua senha deve conter pelo menos 6 caracteres');
-
-  final String message;
-  const ValidatorsErrors(this.message);
-}
--- lib/core/constants/theme/icon_style.dart
+++
@@ -1,4 +0,0 @@
-class AppIconWeights {
-  AppIconWeights._();
-  static const double strong = 1000;
-}
