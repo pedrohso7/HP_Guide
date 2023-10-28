043bf484e4c65798567285b306f9dff7f15d3f17
Pedro Oliveira <pholiveira1998@hotmail.com>
2023-10-27 22:12:07 -0400

    feat: add enum factories to character entity.

--- lib/app/characters/domain/entities/character.dart
+++ lib/app/characters/domain/entities/character.dart
@@ -1,44 +1,5 @@
 import 'package:equatable/equatable.dart';
 
-enum CharacterCategory { staff, student, other }
-
-enum CharacterGender { male, female, other }
-
-enum CharacterHouse { gryffindor, slytherin, ravenclaw, hufflepuff }
-
-enum CharacterSpecies {
-  human,
-  halfuman,
-  goblin,
-  ghost,
-  halfGiant,
-  werewolf,
-  cat,
-  owl,
-  poltergeist,
-  threeHeadedDog,
-  dragon,
-  centaur,
-  houseElf,
-  acromantula,
-  hippogriff,
-  giant,
-}
-
-class Wand extends Equatable {
-  final String wood;
-  final String core;
-  final String size;
-  const Wand({
-    required this.wood,
-    required this.core,
-    required this.size,
-  });
-
-  @override
-  List<Object> get props => [wood, core, size];
-}
-
 class Character extends Equatable {
   final String? id;
   final String name;
@@ -89,4 +50,133 @@ class Character extends Equatable {
         actorName ?? '',
         image ?? '',
       ];
+}
+
+class Wand extends Equatable {
+  final String wood;
+  final String core;
+  final String size;
+  const Wand({
+    required this.wood,
+    required this.core,
+    required this.size,
+  });
+
+  @override
+  List<Object> get props => [wood, core, size];
+}
+
+enum CharacterCategory {
+  staff,
+  student,
+  other;
+
+  factory CharacterCategory.fromBoolValues(bool isStudent, bool isStaff) {
+    if (isStudent) {
+      return CharacterCategory.student;
+    }
+    if (isStaff) {
+      return CharacterCategory.staff;
+    }
+    return CharacterCategory.other;
+  }
+}
+
+enum CharacterGender {
+  male,
+  female,
+  other;
+
+  factory CharacterGender.fromString(String value) {
+    switch (value) {
+      case "male":
+        return CharacterGender.male;
+      case "female":
+        return CharacterGender.female;
+      default:
+        return CharacterGender.other;
+    }
+  }
+}
+
+enum CharacterHouse {
+  gryffindor('gryffindor'),
+  slytherin('slytherin'),
+  ravenclaw('ravenclaw'),
+  hufflepuff('hufflepuff');
+
+  final String name;
+  const CharacterHouse(this.name);
+
+  factory CharacterHouse.fromString(String value) {
+    switch (value) {
+      case "gryffindor":
+        return CharacterHouse.gryffindor;
+      case "slytherin":
+        return CharacterHouse.slytherin;
+      case 'ravenclaw':
+        return CharacterHouse.ravenclaw;
+      default:
+        return CharacterHouse.hufflepuff;
+    }
+  }
+}
+
+enum CharacterSpecies {
+  human,
+  halfHuman,
+  goblin,
+  ghost,
+  halfGiant,
+  werewolf,
+  cat,
+  owl,
+  poltergeist,
+  threeHeadedDog,
+  dragon,
+  centaur,
+  houseElf,
+  acromantula,
+  hippogriff,
+  giant,
+  other;
+
+  factory CharacterSpecies.fromString(String value) {
+    switch (value) {
+      case "human":
+        return CharacterSpecies.human;
+      case "half-human":
+        return CharacterSpecies.halfHuman;
+      case 'goblin':
+        return CharacterSpecies.goblin;
+      case 'ghost':
+        return CharacterSpecies.ghost;
+      case 'half-giant':
+        return CharacterSpecies.halfGiant;
+      case 'werewolf':
+        return CharacterSpecies.werewolf;
+      case 'cat':
+        return CharacterSpecies.cat;
+      case 'owl':
+        return CharacterSpecies.owl;
+      case 'poltergeist':
+        return CharacterSpecies.poltergeist;
+      case 'three-headed dog':
+        return CharacterSpecies.threeHeadedDog;
+      case 'dragon':
+        return CharacterSpecies.dragon;
+      case 'centaur':
+        return CharacterSpecies.centaur;
+      case 'house-elf':
+        return CharacterSpecies.houseElf;
+      case 'acromantula':
+        return CharacterSpecies.acromantula;
+      case 'hippogriff':
+        return CharacterSpecies.hippogriff;
+      case 'giant':
+        return CharacterSpecies.giant;
+      default:
+        return CharacterSpecies.other;
+    }
+  }
 }
