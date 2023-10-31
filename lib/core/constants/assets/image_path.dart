425e48c51aac4d0e4552ebb628d5e300550bccc7
Pedro Oliveira <pholiveira1998@hotmail.com>
2023-10-31 08:46:48 -0400

    feat: add house image assets.

--- lib/core/constants/assets/image_path.dart
+++ lib/core/constants/assets/image_path.dart
@@ -1,63 +1,23 @@
 class AppImages {
   AppImages._();
   static const String logoPath = 'lib/core/assets/images/logo.png';
-  static const String triviaImagePath = 'lib/core/assets/images/trivia.png';
-  static const String starImagePath = 'lib/core/assets/images/star.png';
-  static const String emptyStarImagePath =
-      'lib/core/assets/images/empty_star.png';
-  static const String emptyRandomImagePath =
-      'lib/core/assets/images/empty_shuffle.png';
-  static const String randomImagePath = 'lib/core/assets/images/shuffle.png';
 
-  //Category Icons Images
-  static const String randomIconPath =
-      'lib/core/assets/images/category_icons/changes.png';
-  static const String generalKnowledgeIconPath =
-      'lib/core/assets/images/category_icons/book.png';
-  static const String booksIconPath =
-      'lib/core/assets/images/category_icons/book-stack.png';
-  static const String filmIconPath =
-      'lib/core/assets/images/category_icons/tv.png';
-  static const String musicIconPath =
-      'lib/core/assets/images/category_icons/music-notes.png';
-  static const String musicalsAndTheatresIconPath =
-      'lib/core/assets/images/category_icons/theater.png';
-  static const String televisionIconPath =
-      'lib/core/assets/images/category_icons/news.png';
-  static const String videoGamesIconPath =
-      'lib/core/assets/images/category_icons/controller.png';
-  static const String boardGamesIconPath =
-      'lib/core/assets/images/category_icons/board-games.png';
-  static const String natureIconPath =
-      'lib/core/assets/images/category_icons/dna.png';
-  static const String computersIconPath =
-      'lib/core/assets/images/category_icons/computers.png';
-  static const String mathIconPath =
-      'lib/core/assets/images/category_icons/maths.png';
-  static const String mythologyIconPath =
-      'lib/core/assets/images/category_icons/odin.png';
-  static const String sportsIconPath =
-      'lib/core/assets/images/category_icons/tennis.png';
-  static const String geographyIconPath =
-      'lib/core/assets/images/category_icons/geography.png';
-  static const String historyIconPath =
-      'lib/core/assets/images/category_icons/scroll.png';
-  static const String politicsIconPath =
-      'lib/core/assets/images/category_icons/hammer-and-sickle.png';
-  static const String artIconPath =
-      'lib/core/assets/images/category_icons/art.png';
-  static const String celebritiesIconPath =
-      'lib/core/assets/images/category_icons/celebrity.png';
-  static const String animalsIconPath =
-      'lib/core/assets/images/category_icons/koala.png';
-  static const String vehiclesIconPath =
-      'lib/core/assets/images/category_icons/car.png';
-  static const String comicsIconPath =
-      'lib/core/assets/images/category_icons/comic.png';
-  static const String gadgetsIconPath =
-      'lib/core/assets/images/category_icons/phone-case.png';
-  static const String animeIconPath =
-      'lib/core/assets/images/category_icons/otaku.png';
-  static const String cartoonIconPath =
-      'lib/core/assets/images/category_icons/famous.png';
+  //House mascot's
+  static const String eagleImagePath =
+      'lib/core/assets/images/houses/eagle.png';
+  static const String badgerImagePath =
+      'lib/core/assets/images/houses/badger.png';
+  static const String lionImagePath = 'lib/core/assets/images/houses/lion.png';
+  static const String snakesImagePath =
+      'lib/core/assets/images/houses/snakes.png';
+
+  //House banner's
+  static const String ravenclawBannerImagePath =
+      'lib/core/assets/images/houses/ravenclaw_banner.jpeg';
+  static const String hufflepufsBannerImagePath =
+      'lib/core/assets/images/houses/hufflepufs_banner.jpeg';
+  static const String gryffindorBannerImagePath =
+      'lib/core/assets/images/houses/gryffindor_banner.jpeg';
+  static const String slytherinBannerImagePath =
+      'lib/core/assets/images/houses/slytherin_banner.jpeg';
 }
